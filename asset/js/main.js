802f5eb18012284be7e30185557369a3350905bb
Kharn <kharnthorn@gmail.com>
2020-11-06 16:09:14 +0000

    add comments

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,21 +2,24 @@
 let todoItems = []
 
 const renderTodo = (todo) => {
-  //preserve the list even when refreshing the page
+  //preserve the list even when refreshing the page, Only strings may be stored in the localStorage
   localStorage.setItem('todoItemsRef', JSON.stringify(todoItems))
   const list = document.querySelector('.js-todo-list')
   const item = document.querySelector(`[data-key = '${todo.id}']`)
-
+  //removing the todo item from the DOM
   if (todo.deleted) {
     item.remove()
     return
   }
   // insert done if the checked property is true else its an empty string
   const isChecked = todo.checked ? 'done' : ''
-
+  // Create an `li` element and assign it to `node`
   const node = document.createElement('li')
+  // Set the class attribute
   node.setAttribute('class', `todo-item ${isChecked}`)
+  // Set the data-key attribute to the id of the todo
   node.setAttribute('data-key', todo.id)
+  // Set the contents of the `li` element created above
   node.innerHTML = `
   <input id="${todo.id}" type="checkbox"/>
   <label for="${todo.id}" class="tick js-tick"></label>
@@ -28,12 +31,13 @@ const renderTodo = (todo) => {
     //replace if already exist
     list.replaceChild(node, item)
   } else {
-    //otherwise append to the end of the list
+    //otherwise Append the element to the DOM as the last child of
+    // the element referenced by the `list` variable
     list.append(node)
   }
 }
 
-//function that will create the todo object
+//function that will create the todo object and push it to the todoItems array
 const addTodo = (text) => {
   const todo = {
     text,
@@ -44,23 +48,27 @@ const addTodo = (text) => {
   renderTodo(todo)
 }
 const toggleDone = (key) => {
+  // findIndex is an array method that returns the position of an element in the array.
   const index = todoItems.findIndex((item) => item.id === Number(key))
+  // Locate the todo item in the todoItems array and set its checked property to the opposite. That means, `true` will become `false` and vice versa.
   todoItems[index].checked = !todoItems[index].checked
   renderTodo(todoItems[index])
 }
 
 const deleteTodo = (key) => {
+  // find the corresponding todo object in the todoItems array
   const index = todoItems.findIndex((item) => item.id === Number(key))
+  // create a new object with properties of the current todo item and a 'deleted property which is set to true
   const todo = {
     deleted: true,
     ...todoItems[index],
   }
-  //removed the todo items by filtering it out via the key
+  //removed the todo item from the list by filtering it out via the key
   todoItems = todoItems.filter((item) => item.id !== Number(key))
   renderTodo(todo)
 }
 
-const form = document.querySelector('.js-form')
+const form = document.querySelector('.js-form') //select the form element
 form.addEventListener('submit', (e) => {
   e.preventDefault() //prevent page from refreshing when submit
   const input = document.querySelector('.js-todo-input') // select the input
@@ -74,7 +82,9 @@ form.addEventListener('submit', (e) => {
   }
 })
 
+// Select the entire list
 const list = document.querySelector('.js-todo-list')
+// Add a click event listener to the list which will be inheritted by children
 list.addEventListener('click', (e) => {
   if (e.target.classList.contains('js-tick')) {
     const itemKey = e.target.parentElement.dataset.key
@@ -89,6 +99,7 @@ list.addEventListener('click', (e) => {
 document.addEventListener('DOMContentLoaded', () => {
   const ref = localStorage.getItem('todoItemsRef')
   if (ref) {
+    //convert JSON string back to array and save it in todoItems
     todoItems = JSON.parse(ref)
     todoItems.forEach((t) => {
       renderTodo(t)
